037e0d8d7daff57e69d24f29ed24d190b5d86ae5
xuyanbo03 <610958401@qq.com>
2017-07-05 01:17:22 +0800

    Testing Travis CI

---
+++ hello.test.js
@@ -0,0 +1,10 @@
+var hello=require('./hello');
+
+var expect=require('chai').expect;
+
+describe('输出函数测试',function(){
+
+    it('应该输出Hello World!',function(){
+        expect(hello()).to.equal('Hello World!');
+    });
+});
