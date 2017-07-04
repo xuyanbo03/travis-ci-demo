006072e4dd8c5c449ef402b0c5dc854cdf49aca0
xuyanbo03 <610958401@qq.com>
2017-07-05 01:24:24 +0800

    Testing Travis CI

--- hello.test.js
+++ hello.test.js
@@ -5,7 +5,6 @@ var hello=require('./hello');
 var expect=require('chai').expect;
 
 describe('输出函数测试',function(){
-
     it('应该输出Hello World!',function(){
         expect(hello()).to.equal('Hello World!');
     });
